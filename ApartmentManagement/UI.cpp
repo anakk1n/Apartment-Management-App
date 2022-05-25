94752955328707cc6d1f21c9a8d869a54ebbe035
anakk1n <65091385+anakk1n@users.noreply.github.com>
2022-05-25 22:22:13 +0300

    Update UI.cpp

--- ApartmentManagement/UI.cpp
+++ ApartmentManagement/UI.cpp
@@ -163,3 +163,142 @@ void UI::UIcautaApartament()
 	}
 
 }
+//UI pentru filtrare dupa tip apartament
+void UI::UIfiltrareTipApartament()
+{
+	string  apartament;
+	cout << "tip apartament=";
+	getline(cin, apartament, '\n');
+	vector<Locatar>locatari = srv.filtrareTipApartament(apartament);
+	if (locatari.size() == 0)
+	{
+		cout << "lista goala\n";
+		return;
+	}
+	for (const auto& l : locatari)
+		cout << " Apartament:" << l.getApartament() << " Nume:" << l.getNume() << " Suprafata:" << l.getSuprafata() << " mp Tip Apartament:" << l.getTipApartament() << "\n";
+}
+//UI pentru filtrare dupa suprafata
+void UI::UIfiltrareSuprafata()
+{
+	string suprafata;
+	int suprafata1;
+	cout << "suprafata=";
+	getline(cin, suprafata, '\n');
+	try {
+		suprafata1 = stoi(suprafata);
+	}
+	catch (const std::invalid_argument) {
+		cout << "Valoare invalida\n";
+		return;
+	}
+	vector<Locatar>locatari = srv.filtrareSuprafata(suprafata1);
+	if (locatari.size() == 0)
+	{
+		cout << "lista goala\n";
+		return;
+	}
+	for (const auto& l : locatari)
+		cout << " Apartament:" << l.getApartament() << " Nume:" << l.getNume() << " Suprafata:" << l.getSuprafata() << " mp Tip Apartament:" << l.getTipApartament() << "\n";
+
+}
+//UI pentru sortare dupa nume
+void UI::UIsortareNume()
+{
+	vector<Locatar> locatari = srv.sortareNume();
+	if (locatari.size() == 0)
+	{
+		cout << "lista goala\n";
+		return;
+	}
+	for (const auto& l : locatari)
+		cout << " Apartament:" << l.getApartament() << " Nume:" << l.getNume() << " Suprafata:" << l.getSuprafata() << " mp Tip Apartament:" << l.getTipApartament() << "\n";
+
+}
+
+//UI pentru sortare dupa suprafata
+void UI::UIsortareSuprafata()
+{
+	vector<Locatar> locatari = srv.sortareSuprafata();
+	if (locatari.size() == 0)
+	{
+		cout << "lista goala\n";
+		return;
+	}
+	for (const auto& l : locatari)
+		cout << " Apartament:" << l.getApartament() << " Nume:" << l.getNume() << " Suprafata:" << l.getSuprafata() << " mp Tip Apartament:" << l.getTipApartament() << "\n";
+
+}
+
+//UI pentru sortare  dupa tip si suprafata
+void UI::UIsortareTipSuprafata()
+{
+	vector<Locatar> locatari = srv.sortareApartamentSuprafata();
+	if (locatari.size() == 0)
+	{
+		cout << "lista goala\n";
+		return;
+	}
+	for (const auto& l : locatari)
+		cout << " Apartament:" << l.getApartament() << " Nume:" << l.getNume() << " Suprafata:" << l.getSuprafata() << " mp Tip Apartament:" << l.getTipApartament() << "\n";
+
+}
+
+void UI::UIadaugaListaNotificari()
+{
+	string apartament;
+	int apartament1;
+	cout << "apartament=";
+	getline(cin, apartament, '\n');
+
+	try {
+		apartament1 = stoi(apartament);
+	}
+	catch (const std::invalid_argument) {
+		cout << "Valoare invalida\n";
+		return;
+	}
+	try {
+		srv.adaugaLista(apartament1);
+	}
+	catch (const listaException& msg) {
+		cout << msg.getMesaj();
+	}
+	catch (const RepoException& msg) {
+		cout << msg.getMesaj();
+	}
+	const int total = srv.nrApartamenteLista();
+	cout << "nr aparteamente din lista de notificari:" << total;
+
+}
+
+void UI::UIgolesteLista() {
+	srv.golesteLista();
+	const int total = srv.nrApartamenteLista();
+	cout << "nr aparteamente din lista de notificari:" << total;
+}
+
+void UI::UIgenereazaLista()
+{
+	string numar;
+	int numar1;
+	cout << "apartamente=";
+	getline(cin, numar, '\n');
+
+	try {
+		numar1 = stoi(numar);
+	}
+	catch (const std::invalid_argument) {
+		cout << "Valoare invalida\n";
+		return;
+	}
+	try {
+		srv.genereazaLista(numar1);
+	}
+	catch (const listaException& msg) {
+		cout << msg.getMesaj();
+	}
+	const int total = srv.nrApartamenteLista();
+	cout << "nr aparteamente din lista de notificari:" << total;
+}
+
